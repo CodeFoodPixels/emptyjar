0319df9f66dbf9bad39668fb3398211e45d0a0b7
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-09-19 20:39:39 +0100

    Add api

--- data.js
+++ data.js
@@ -7,27 +7,94 @@ const db = new sqlite3.Database(dbFile);
 db.serialize(() => {
   if (!dbFileExists) {
     db.run(
-      "CREATE TABLE hits (url TEXT, browser TEXT, operating_system TEXT, device_type TEXT, country TEXT, timestamp INT)"
+      "CREATE TABLE hits (url TEXT, browser TEXT, browser_version REAL, operating_system TEXT, operating_system_version REAL, device_type TEXT, country TEXT, timestamp INT)"
     );
   }
 });
 
 module.exports = {
-  logHit(url, browser, operating_system, device_type, country) {
+  logHit(data) {
     const hitData = [
-      url,
-      browser,
-      operating_system,
-      device_type,
-      country,
-      Math.floor(Date.now()/1000)
+      data.url,
+      data.browser,
+      data.browser_version,
+      data.operating_system,
+      data.operating_system_version,
+      data.device_type,
+      data.country,
+      Math.floor(Date.now() / 1000)
     ];
-    
+
     db.serialize(() => {
       db.run(
-        "INSERT INTO hits (url, browser, operating_system, device_type, country, timestamp) VALUES (?, ?, ?, ?, ?, ?)",
+        "INSERT INTO hits (url, browser, browser_version, operating_system, operating_system_version, device_type, country, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
         hitData
       );
     });
+  },
+
+  getHits(params) {
+    const queryParts = [];
+    const queryValues = [];
+
+    if (params.url) {
+      queryParts.push("url LIKE ?");
+      queryValues.push(params.url);
+    }
+
+    if (params.operating_system) {
+      queryParts.push("operating_system LIKE ?");
+      queryValues.push(params.operating_system);
+    }
+
+    if (params.operating_system_version) {
+      queryParts.push("operating_system_version=?");
+      queryValues.push(params.operating_system_version);
+    }
+
+    if (params.browser) {
+      queryParts.push("browser LIKE ?");
+      queryValues.push(params.browser);
+    }
+
+    if (params.browser_version) {
+      queryParts.push("browser_version=?");
+      queryValues.push(params.browser_version);
+    }
+
+    if (params.device_type) {
+      queryParts.push("device_type LIKE ?");
+      queryValues.push(params.device_type);
+    }
+
+    if (params.country) {
+      queryParts.push("country LIKE ?");
+      queryValues.push(params.country);
+    }
+
+    if (params.time_from) {
+      queryParts.push("timestamp >= ?");
+      queryValues.push(params.time_from);
+    }
+
+    if (params.time_to) {
+      queryParts.push("timestamp <= ?");
+      queryValues.push(params.time_to);
+    }
+
+    let query = "SELECT * from hits";
+
+    if (queryParts.length > 0) {
+      query = `${query} WHERE ${queryParts.join(" AND ")}`;
+    }
+
+    return new Promise((resolve, reject) => {
+      db.all(query, queryValues, function(err, rows) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
   }
-}
+};
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
 const uaParser = require("ua-parser-js");
-const geoip = require('geoip-lite');
-const data = require('./data.js');
+const geoip = require("geoip-lite");
+const data = require("./data.js");
 
 const app = express();
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 function nocache(req, res, next) {
   res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
@@ -17,28 +17,105 @@ function nocache(req, res, next) {
 
 app.get("/ping.png", nocache, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "ping.png"));
-  const url = req.get('Referrer') || req.query.fallback;
+  const url = req.get("Referrer") || req.query.fallback;
   if (url) {
-    const userAgent = uaParser(req.get('User-Agent'));
-    
-    const browser = `${userAgent.browser.name} ${userAgent.browser.version}`;
-    const os = `${userAgent.os.name} ${userAgent.os.version}`;
-    const deviceType = userAgent.device.type || "Unknown";
+    const userAgent = uaParser(req.get("User-Agent"));
+
+    const device_type = userAgent.device.type || "Unknown";
 
     const geo = geoip.lookup(req.ip);
 
     let country = "Unknown";
-    
+
     if (geo && geo.country) {
       country = geo.country;
     }
 
-    data.logHit(url, browser, os, deviceType, country);
+    data.logHit({
+      url,
+      browser: userAgent.browser.name,
+      browser_version: userAgent.browser.version,
+      operating_system: userAgent.os.name,
+      operating_system_version: userAgent.os.version,
+      device_type,
+      country
+    });
   }
 });
 
+app.get("/api/hits", nocache, (req, res) => {
+  const params = {};
+
+  if (req.query.url) {
+    params.url = req.query.url;
+  }
+
+  if (req.query.operating_system) {
+    params.operating_system = req.query.operating_system;
+  }
+
+  if (req.query.operating_system_version) {
+    if (!req.query.operating_system) {
+      res.status(500);
+      return res.json({
+        message:
+          "operating_system must be defined when searching for operating_system_version"
+      });
+    }
+
+    params.operating_system_version = req.query.operating_system_version;
+  }
+
+  if (req.query.browser) {
+    params.browser = req.query.browser;
+  }
+
+  if (req.query.browser_version) {
+    if (!req.query.browser) {
+      res.status(500);
+      return res.json({
+        message: "browser must be defined when searching for browser_version"
+      });
+    }
+
+    params.browser_version = req.query.browser_version;
+  }
+
+  if (req.query.device_type) {
+    params.device_type = req.query.device_type;
+  }
+
+  if (req.query.country) {
+    params.country = req.query.country;
+  }
+
+  if (req.query.time_from) {
+    params.time_from = req.query.time_from;
+  }
+
+  if (req.query.time_to) {
+    params.time_to = req.query.time_to;
+  }
+
+  if (req.query.country) {
+    params.country = req.query.country;
+  }
+
+  data.getHits(params).then(data => {
+    if (data.length === 0) {
+      res.status(404);
+    }
+    res.json(data);
+  });
+});
+
+app.get("/api/teapot", (req, res) => {
+  res.status(418);
+  res.json({ message: "I'm a little teapot..." });
+});
+
 app.get("/", nocache, (req, res) => {
-  res.end('<img src="/ping.png">')
+  res.end('<img src="/ping.png">');
 });
 
 app.listen(process.env.PORT, () => {
