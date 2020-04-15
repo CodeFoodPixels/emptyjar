5e55c45a07f8d676a57da6fe159ac9a0ff1c70c2
Luke Bonaccorsi <luke.bonaccorsi@and.digital>
2020-04-15 16:47:12 +0100

    Remove remnant of redux

--- client/index.js
+++ client/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App.js";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import { StateProvider } from "./context.js";
 
