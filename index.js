7b37d249a0a0a22d06859fa255ec2ecb36ac4e70
Luke Bonaccorsi <luke@lukeb.co.uk>
2021-05-13 10:56:50 +0100

    Update to use JS file instead of pure image

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require("http");
 const path = require("path");
-const url = require("url");
 const fs = require("fs");
 const crypto = require("crypto");
 const bowser = require("bowser");
@@ -13,29 +12,32 @@ const port = process.env.PORT || 8080;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
+  const reqUrl = new URL(getRequestURL(req));
+
+  res.setHeader(
+    "Cache-Control",
+    "private, no-cache, no-store, must-revalidate"
+  );
+  res.setHeader("Expires", "-1");
+  res.setHeader("Pragma", "no-cache");
+
+  const pathName =
+    reqUrl.pathname.length > 1
+      ? reqUrl.pathname.replace(/\/$/, "")
+      : reqUrl.pathname;
+
   if (req.method === "GET") {
-    const reqUrl = url.parse(req.url, true);
-
-    req.query = reqUrl.query;
-
-    res.setHeader(
-      "Cache-Control",
-      "private, no-cache, no-store, must-revalidate"
-    );
-    res.setHeader("Expires", "-1");
-    res.setHeader("Pragma", "no-cache");
-
-    const pathName =
-      reqUrl.pathname.length > 1
-        ? reqUrl.pathname.replace(/\/$/, "")
-        : reqUrl.pathname;
+    req.query = reqUrl.searchParams;
 
     switch (pathName) {
       case "/ping.png":
         ping(req, res);
         break;
       case "/ping":
-        res.end('<img src="/ping.png">');
+        res.end('<script src="/ping.js"></script>');
+        break;
+      case "/beacon":
+        beacon(req, res);
         break;
       case "/api/hits":
         hits(req, res);
@@ -50,32 +52,69 @@ server.on("request", (req, res) => {
         staticFile(pathName, req, res);
         break;
     }
+  } else if (req.method === "POST") {
+    switch (pathName) {
+      case "/beacon":
+        beacon(req, res);
+        break;
+      default:
+        res.statusCode = 404;
+        res.end("Not found");
+        break;
+    }
   }
 });
 
-async function ping(req, res) {
+function ping(req, res) {
   sendFile(res, path.join("public", "img", "ping.png"));
 
-  const referrer = req.headers["referrer"] || req.headers["referer"];
-  const requestURL = referrer
-    ? referrer
-    : req.query.fallback
-    ? `[FALLBACK] ${req.query.fallback}`
-    : undefined;
+  const ip =
+    (req.headers["x-forwarded-for"] || "").split(",").shift() ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
 
-  if (requestURL && urlChecker(requestURL)) {
-    const userAgentString = req.headers["user-agent"];
-    const userAgent = bowser.parse(userAgentString);
+  const data =
+    JSON.parse(
+      decodeURIComponent(new URL(getRequestURL(req)).searchParams.get("data"))
+    ) || {};
+
+  data.ip = ip;
+  data.ua = req.headers["user-agent"];
+
+  processHit(data);
+}
+
+function beacon(req, res) {
+  res.end();
+
+  const ip =
+    (req.headers["x-forwarded-for"] || "").split(",").shift() ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+
+  let body = "";
+  req.on("data", chunk => {
+    body += chunk.toString(); // convert Buffer to string
+  });
+  req.on("end", () => {
+    const data = JSON.parse(body);
+
+    data.ip = ip;
+    data.ua = req.headers["user-agent"];
+
+    processHit(data);
+  });
+}
+
+async function processHit(hit) {
+  if (hit.url && urlChecker(hit.url)) {
+    const userAgent = bowser.parse(hit.ua);
 
     const device_type = userAgent.platform.type || "Unknown";
 
-    const ip =
-      (req.headers["x-forwarded-for"] || "").split(",").shift() ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
-
-    const geo = geoip.lookup(ip);
+    const geo = geoip.lookup(hit.ip);
 
     let country = "Unknown";
 
@@ -83,7 +122,7 @@ async function ping(req, res) {
       country = geo.country;
     }
 
-    const parsedURL = url.parse(requestURL);
+    const parsedURL = new URL(hit.url);
     const today = new Date();
     const year = today.getUTCFullYear();
     const month = (today.getUTCMonth() + 1).toString().padStart(2, "0");
@@ -91,8 +130,8 @@ async function ping(req, res) {
       .getUTCDate()
       .toString()
       .padStart(2, "0");
-    const pageHitData = `${year}${month}${day}${parsedURL.host}/${parsedURL.pathname}${req.ip}${userAgentString}`;
-    const siteHitData = `${year}${month}${day}${parsedURL.host}${req.ip}${userAgentString}`;
+    const pageHitData = `${year}${month}${day}${parsedURL.host}/${parsedURL.pathname}${hit.ip}${hit.ua}`;
+    const siteHitData = `${year}${month}${day}${parsedURL.host}${hit.ip}${hit.ua}`;
     const pageHitSignature = crypto
       .createHash("sha256")
       .update(pageHitData)
@@ -127,7 +166,7 @@ async function ping(req, res) {
       "Unknown";
 
     data.logHit({
-      url: requestURL,
+      url: hit.url,
       browser,
       operating_system,
       device_type,
@@ -143,46 +182,46 @@ function hits(req, res) {
 
   const params = {};
 
-  if (req.query.url) {
-    params.url = req.query.url;
+  if (req.query.get("url")) {
+    params.url = req.query.get("url");
   }
 
-  if (req.query.operating_system) {
-    params.operating_system = req.query.operating_system;
+  if (req.query.get("operating_system")) {
+    params.operating_system = req.query.get("operating_system");
   }
 
-  if (req.query.browser) {
-    params.browser = req.query.browser;
+  if (req.query.get("browser")) {
+    params.browser = req.query.get("browser");
   }
 
-  if (req.query.device_type) {
-    params.device_type = req.query.device_type;
+  if (req.query.get("device_type")) {
+    params.device_type = req.query.get("device_type");
   }
 
-  if (req.query.country) {
-    params.country = req.query.country;
+  if (req.query.get("country")) {
+    params.country = req.query.get("country");
   }
 
-  if (req.query.page_hit_unique) {
+  if (req.query.get("page_hit_unique")) {
     params.page_hit_unique =
-      req.query.page_hit_unique === "true" ? true : false;
+      req.query.get("page_hit_unique") === "true" ? true : false;
   }
 
-  if (req.query.site_hit_unique) {
+  if (req.query.get("site_hit_unique")) {
     params.site_hit_unique =
-      req.query.site_hit_unique === "true" ? true : false;
+      req.query.get("site_hit_unique") === "true" ? true : false;
   }
 
-  if (req.query.from) {
-    params.from = req.query.from;
+  if (req.query.get("from")) {
+    params.from = req.query.get("from");
   }
 
-  if (req.query.to) {
-    params.to = req.query.to;
+  if (req.query.get("to")) {
+    params.to = req.query.get("to");
   }
 
-  if (req.query.country) {
-    params.country = req.query.country;
+  if (req.query.get("country")) {
+    params.country = req.query.get("country");
   }
 
   data.getHits(params).then(data => {
@@ -251,6 +290,12 @@ function sendJSON(res, data) {
   } else {
     res.end(data);
   }
+}
+
+function getRequestURL(req) {
+  var secure =
+    req.connection.encrypted || req.headers["x-forwarded-proto"] === "https";
+  return "http" + (secure ? "s" : "") + "://" + req.headers.host + req.url;
 }
 
 server.listen(port, () => {
---
+++ public/ping.js
@@ -0,0 +1,32 @@
+(function() {
+  const scriptUrl = new URL(document.currentScript.src);
+  const analyticsUrl = scriptUrl.origin;
+
+  function trackHit() {
+    const data = {
+      url: window.location.href
+    };
+
+    if (navigator.sendBeacon) {
+      navigator.sendBeacon(analyticsUrl + "/beacon", JSON.stringify(data));
+    } else {
+      const img = document.createElement("img");
+      img.setAttribute("alt", "");
+      img.setAttribute("aria-hidden", "true");
+      img.style.position = "absolute";
+      img.src =
+        analyticsUrl + "/ping?data=" + encodeURIComponent(JSON.stringify(data));
+      img.addEventListener("load", function() {
+        img.parentNode.removeChild(img);
+      }),
+        img.addEventListener("error", function() {
+          img.parentNode.removeChild(img);
+        }),
+        document.body.appendChild(img);
+    }
+  }
+
+  setTimeout(function() {
+    trackHit();
+  });
+})();
