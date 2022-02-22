019b70542a91c887f0ff8c20c96d01a93661d4b8
Bonaccorsi, Luke <luke.bonaccorsi@sky.uk>
2022-02-22 19:03:59 +0000

    Refactor capture endpoints

--- server/routes/capture.js
+++ server/routes/capture.js
@@ -6,79 +6,77 @@ const urlChecker = require("../urlChecker.js");
 const { removeTrailingSlashes } = require("../helpers.js");
 const data = require("../data.js");
 
-function ping(req, res) {
-  sendFile(res, path.join("..", "public", "img", "ping.png"));
-
-  const ip =
+function getIP(req) {
+  return (
     (req.headers["x-forwarded-for"] || "").split(",").shift() ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
+    req.connection.socket.remoteAddress
+  );
+}
 
-  const data =
-    JSON.parse(decodeURIComponent(req.requestUrl.searchParams.get("data"))) ||
-    {};
+function ping(req, res) {
+  sendFile(res, path.join("..", "public", "img", "ping.png"));
 
-  data.ip = ip;
-  data.ua = req.headers["user-agent"];
+  try {
+    const data = JSON.parse(
+      decodeURIComponent(req.requestUrl.searchParams.get("data"))
+    );
 
-  processHit(data);
+    data.ip = getIP(getIP);
+    data.ua = req.headers["user-agent"];
+
+    processHit(data);
+  } catch (e) {}
 }
 
 function beacon(req, res) {
   res.end();
-
-  const ip =
-    (req.headers["x-forwarded-for"] || "").split(",").shift() ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
 
   let body = "";
   req.on("data", chunk => {
     body += chunk.toString(); // convert Buffer to string
   });
   req.on("end", () => {
-    const data = JSON.parse(body);
+    try {
+      const data = JSON.parse(body);
 
-    data.ip = ip;
-    data.ua = req.headers["user-agent"];
-
-    processHit(data);
+      data.ip = getIP(req);
+      data.ua = req.headers["user-agent"];
+      processHit(data);
+    } catch (e) {}
   });
 }
 
 async function processHit(hit) {
   if (hit.url && urlChecker(hit.url)) {
+    const hitData = {
+      country: "Unknown",
+      referrer: ""
+    };
+
     const userAgent = bowser.parse(hit.ua);
+    hitData.device_type = userAgent.platform.type || "Unknown";
 
-    const device_type = userAgent.platform.type || "Unknown";
+    hitData.url = removeTrailingSlashes(hit.url);
 
-    const geo = geoip.lookup(hit.ip);
-
-    let country = "Unknown";
-
-    hit.url = removeTrailingSlashes(hit.url);
     if (hit.r) {
       try {
         const referrer = new URL(hit.r);
 
         if (referrer.origin && referrer.origin !== "null") {
-          hit.r = referrer.host;
+          hitData.referrer = referrer.host;
         } else if (referrer.protocol === "android-app:") {
-          hit.r = referrer.href;
+          hitData.referrer = referrer.href;
         } else {
-          hit.r = "";
+          hitData.referrer = "";
         }
-      } catch (e) {
-        hit.r = "";
-      }
-    } else {
-      hit.r = "";
+      } catch (e) {}
     }
 
+    const geo = geoip.lookup(hit.ip);
     if (geo && geo.country) {
-      country = geo.country;
+      hitData.country = geo.country;
     }
 
     const parsedURL = new URL(hit.url);
@@ -89,30 +87,28 @@ async function processHit(hit) {
       .getUTCDate()
       .toString()
       .padStart(2, "0");
+
     const pageHitData = `${year}${month}${day}${parsedURL.host}/${parsedURL.pathname}${hit.ip}${hit.ua}${hit.r}`;
-    const siteHitData = `${year}${month}${day}${parsedURL.host}${hit.ip}${hit.ua}`;
     const pageHitSignature = crypto
       .createHash("sha256")
       .update(pageHitData)
       .digest("hex");
+    hitData.page_hit_unique = await data.pageHitUnique(pageHitSignature);
+    if (hitData.page_hit_unique) {
+      data.logPageHitSignature(pageHitSignature);
+    }
+
+    const siteHitData = `${year}${month}${day}${parsedURL.host}${hit.ip}${hit.ua}`;
     const siteHitSignature = crypto
       .createHash("sha256")
       .update(siteHitData)
       .digest("hex");
-
-    const page_hit_unique = await data.pageHitUnique(pageHitSignature);
-
-    if (page_hit_unique) {
-      data.logPageHitSignature(pageHitSignature);
-    }
-
-    const site_hit_unique = await data.siteHitUnique(siteHitSignature);
-
-    if (site_hit_unique) {
+    hitData.site_hit_unique = await data.siteHitUnique(siteHitSignature);
+    if (hitData.site_hit_unique) {
       data.logSiteHitSignature(siteHitSignature);
     }
 
-    const browser =
+    hitData.browser =
       `${userAgent.browser.name || ""} ${userAgent.browser.version ||
         ""}`.trim() || "Unknown";
 
@@ -120,20 +116,11 @@ async function processHit(hit) {
       userAgent.os.name.toLowerCase() === "windows"
         ? userAgent.os.versionName
         : userAgent.os.version;
-    const operating_system =
+    hitData.operating_system =
       `${userAgent.os.name || ""} ${operating_system_version || ""}`.trim() ||
       "Unknown";
 
-    data.logHit({
-      url: hit.url,
-      referrer: hit.r || "",
-      browser,
-      operating_system,
-      device_type,
-      country,
-      page_hit_unique,
-      site_hit_unique
-    });
+    data.logHit(hitData);
   }
 }
 
