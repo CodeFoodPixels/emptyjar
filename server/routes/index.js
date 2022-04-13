0895f8ac89ff4fc2ee87c6792af221cd22def5b0
Bonaccorsi, Luke <luke.bonaccorsi@sky.uk>
2022-04-13 18:33:40 +0100

    Remove old beacon route

--- server/routes/index.js
+++ server/routes/index.js
@@ -18,7 +18,6 @@ module.exports = {
   GET: {
     "/ping": ping,
     "/test": (req, res) => res.end('<script src="/ping.js"></script>'),
-    "/beacon": beacon,
     "/api/hits": hits,
     "/api/teapot": teapot,
     "/": (req, res) => res.sendFile(path.join("..", "public", "index.html")),
